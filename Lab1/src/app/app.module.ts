81efbe0235ebc3fdddde76c55daf45fc3ad75c1a
Vlad Varvashenko <vlad19972017@gmail.com>
2018-09-25 21:44:32 +0300

    Resolved first lab

--- Lab1/src/app/app.module.ts
+++ Lab1/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputsOutputsComponent } from './inputAreas/inputs-outputs/inputs-outputs.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { InputsOutputsComponent } from './inputAreas/inputs-outputs/inputs-outpu
     InputsOutputsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
